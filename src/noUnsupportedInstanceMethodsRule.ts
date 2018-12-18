84aea4f20f1f105745fa3d6fd785fba5e1ebc90a
Travis Jefferson <traviscjefferson@gmail.com>
2018-12-17 18:15:19 -0600

    Super silly rule logic to fix hardcoded test

--- src/noUnsupportedInstanceMethodsRule.ts
+++ src/noUnsupportedInstanceMethodsRule.ts
@@ -8,9 +8,24 @@ export class Rule extends Lint.Rules.TypedRule {
   ): Lint.RuleFailure[] {
     return this.applyWithFunction(
       sourceFile,
-      () => {},
+      walk,
       undefined,
       program.getTypeChecker()
     );
   }
+}
+
+function walk(ctx: Lint.WalkContext<void>, _checker: ts.TypeChecker) {
+  function callback(nodeObj: ts.Node): void {
+    if (nodeObj.kind === ts.SyntaxKind.PropertyAccessExpression) {
+      const node = nodeObj as ts.PropertyAccessExpression;
+      if (node.getText() === "[1, 2, 3].includes") {
+        ctx.addFailureAtNode(node, `Array.prototype.includes is not allowed!`);
+      }
+    }
+
+    return ts.forEachChild(nodeObj, callback);
+  }
+
+  return ts.forEachChild(ctx.sourceFile, callback);
 }
