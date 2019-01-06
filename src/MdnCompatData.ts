3944d8074e18de46a5755daf6fa8ace4bc6501f8
Travis Jefferson <traviscjefferson@gmail.com>
2019-01-05 21:34:57 -0600

    Define a result type for mdn support lookup
    And clean up passing messages along for debug when:
    1) There's no MDN namespace that matches the LHS expression
    2) There is a MDN namespace but digging into it for the support entry raises an
    error (presumable it's an accidental match on the MDN namespace name?)

--- src/MdnCompatData.ts
+++ src/MdnCompatData.ts
@@ -1,3 +1,5 @@
+import { debug } from './utils';
+
 const MdnData = require('mdn-browser-compat-data');
 const mdnData: any = MdnData;
 
@@ -5,26 +7,52 @@ const mdnData: any = MdnData;
 type FunctionLookupData = { objectType: string; functionName: string | null };
 type BrowserTarget = { browserName: string; version: number };
 
+type QuerySuccess = {
+  type: 'success';
+  supportEntry: { [key: string]: { version_added: any } };
+};
+type NoResultsFailure = {
+  type: 'no-mdn-namespace';
+  message: string;
+};
+type LookupFailure = {
+  type: 'lookup-failure';
+  message: string;
+};
+type QueryResult = QuerySuccess | NoResultsFailure | LookupFailure;
+
 /* E.g. look for the Array.from compatabilty entry by passing 'Array'
  * and 'from' as parameters */
-export const query = ({ objectType, functionName }: FunctionLookupData) => {
+export const query = ({
+  objectType,
+  functionName
+}: FunctionLookupData): QueryResult => {
   try {
     if (mdnData.javascript.builtins[objectType]) {
-      return functionName === null
-        ? mdnData.javascript.builtins[objectType].__compat.support
-        : mdnData.javascript.builtins[objectType][functionName].__compat
-            .support;
+      const supportEntry =
+        functionName === null
+          ? mdnData.javascript.builtins[objectType].__compat.support
+          : mdnData.javascript.builtins[objectType][functionName].__compat
+              .support;
+      return { type: 'success', supportEntry };
     }
 
     if (mdnData.api[objectType]) {
-      return functionName === null
-        ? mdnData.api[objectType].__compat.support
-        : mdnData.api[objectType][functionName].__compat.support;
+      const supportEntry =
+        functionName === null
+          ? mdnData.api[objectType].__compat.support
+          : mdnData.api[objectType][functionName].__compat.support;
+      return { type: 'success', supportEntry };
     }
+    return {
+      type: 'no-mdn-namespace',
+      message: 'No MDN namespace was found for ${objectType}.${functionName}'
+    };
   } catch (_err) {
-    throw new Error(
-      `Couldn't look up ${objectType}.${functionName} in MDN browser support data`
-    );
+    return {
+      type: 'lookup-failure',
+      message: `Couldn't look up ${objectType}.${functionName} in MDN browser support data`
+    };
   }
 };
 
@@ -34,11 +62,19 @@ export const isFeatureSupported = (
   browserTarget: BrowserTarget
 ) => {
   const { browserName, version } = browserTarget;
-  const supportEntry = query(functionLookupData);
+  const queryResult = query(functionLookupData);
+
+  if (
+    queryResult.type === 'lookup-failure' ||
+    queryResult.type === 'no-mdn-namespace'
+  ) {
+    debug(queryResult.message);
+    return true;
+  }
 
   // `version_added` can be true, false, null, or a numeric version
   // (unfortunately represented as a string)
-  const versionAdded = supportEntry[browserName].version_added;
+  const versionAdded = queryResult.supportEntry[browserName].version_added;
 
   if (versionAdded === true) {
     return true;
